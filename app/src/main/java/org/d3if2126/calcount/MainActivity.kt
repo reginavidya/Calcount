2c6590b4fa4699cb012f96bb4d20dfdbb05b19e6
ReginaVidyaDestiani <reginavidya@student.telkomuniversity.ac.id>
2022-03-31 13:03:42 +0700

    perhitungan diskon dan input valid

--- app/src/main/java/org/d3if2126/calcount/MainActivity.kt
+++ app/src/main/java/org/d3if2126/calcount/MainActivity.kt
@@ -2,10 +2,45 @@ package org.d3if2126.calcount
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import org.d3if2126.calcount.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener { hitungDiskon()}
+        binding.button2.setOnClickListener { reset()}
+    }
+    private fun hitungDiskon(){
+        val harga = binding.editHarga.text.toString()
+        if (TextUtils.isEmpty(harga)) {
+            Toast.makeText(this, R.string.harga_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val diskon = binding.editDiskon.text.toString()
+        if (TextUtils.isEmpty(diskon)) {
+            Toast.makeText(this, R.string.diskon_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val hitungDiskon = harga.toFloat() * (diskon.toFloat() /100)
+        val hasilDiskon = harga.toFloat() - hitungDiskon
+
+        binding.editHasil.setText("Rp."+ hitungDiskon.toInt())
+        binding.editTotal.setText("Rp."+ hasilDiskon.toInt())
+    }
+    private fun reset(){
+
+        binding.editHarga.setText("")
+        binding.editDiskon.setText("")
+        binding.editHasil.setText("")
+        binding.editTotal.setText("")
+
     }
 }
