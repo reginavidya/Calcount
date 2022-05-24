a2ebfa5542803b73930713819e368b23e8549767
ReginaVidyaDestiani <reginavidya@student.telkomuniversity.ac.id>
2022-05-24 11:54:08 +0700

    menyimpan preferences menggunakan DataStore

--- app/src/main/java/org/d3if2126/calcount/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if2126/calcount/ui/histori/HistoriFragment.kt
@@ -2,17 +2,27 @@ package org.d3if2126.calcount.ui.histori
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.launch
 import org.d3if2126.calcount.R
+import org.d3if2126.calcount.data.SettingDataStore
+import org.d3if2126.calcount.data.dataStore
 import org.d3if2126.calcount.databinding.FragmentHistoriBinding
 import org.d3if2126.calcount.db.DiskonDb
 
 class HistoriFragment : Fragment() {
 
+    private lateinit var layoutDataStore: SettingDataStore
+    private var isLinearLayoutManager = true
     private val viewModel: HistoriViewModel by lazy {
         val db = DiskonDb.getInstance(requireContext())
         val factory = HistoriViewModelFactory(db.dao)
@@ -35,25 +45,66 @@ class HistoriFragment : Fragment() {
             adapter = myAdapter
             setHasFixedSize(true)
         }
+        layoutDataStore = SettingDataStore(requireContext().dataStore)
+        layoutDataStore.preferenceFlow.asLiveData()
+            .observe(viewLifecycleOwner, { value ->
+                isLinearLayoutManager = value
+                chooseLayout()
+                activity?.invalidateOptionsMenu()
+            })
         viewModel.data.observe(viewLifecycleOwner, {
             binding.emptyView.visibility = if (it.isEmpty())
                 View.VISIBLE else View.GONE
             myAdapter.submitList(it)
         })
     }
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            binding.recyclerView.layoutManager =
+                LinearLayoutManager(this.requireContext())
+        } else {
+            binding.recyclerView.layoutManager =
+                GridLayoutManager(this.requireContext(), 2)
+        }
+    }
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null) return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(requireContext(),
+                    R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(requireContext(),
+                R.drawable.ic_linear_layout)
+    }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.histori_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+
+                lifecycleScope.launch {
+                    layoutDataStore.saveLayoutToPreferencesStore(
+                        isLinearLayoutManager, requireContext()
+                    )
+                }
+                chooseLayout()
+                setIcon(item)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
         if (item.itemId == R.id.menu_hapus) {
             hapusData()
             return true
         }
         return super.onOptionsItemSelected(item)
     }
-
     private fun hapusData() {
         MaterialAlertDialogBuilder(requireContext())
             .setMessage(R.string.konfirmasi_hapus)
